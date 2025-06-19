9d37311e1bd371930bc5652704dfac9b55679871
Nischal Tandukar <nischaltan@gmail.com>
2025-06-19 22:16:22 +0545

    added caterer and decorator apis

--- Backend/src/config/model.associations.js
+++ Backend/src/config/model.associations.js
@@ -1,6 +1,8 @@
 const User = require("../modules/user/user.model");
 const Photographer = require("../modules/photographer/photographer.model");
 const MakeupArtist = require("../modules/makeupartist/makeupartist.model");
+const Caterer = require("../modules/caterer/caterer.model");
+const Decorator = require("../modules/decorator/decorator.model");
 
 // User - Photographer
 User.hasOne(Photographer, {
@@ -32,8 +34,38 @@ MakeupArtist.belongsTo(User, {
   onUpdate: "CASCADE",
 });
 
+// User - Caterer
+User.hasOne(Caterer, {
+  foreignKey: "userId",
+  as: "caterer",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+Caterer.belongsTo(User, {
+  foreignKey: "userId",
+  as: "user",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
+// User - Decorator
+User.hasOne(Decorator, {
+  foreignKey: "userId",
+  as: "decorator",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+Decorator.belongsTo(User, {
+  foreignKey: "userId",
+  as: "user",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+
 module.exports = {
   User,
   Photographer,
   MakeupArtist,
+  Caterer,
+  Decorator,
 };
--- Backend/src/config/router.config.js
+++ Backend/src/config/router.config.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const authRoutes = require("../modules/auth/auth.router"); // adjust path as needed
+const catererRouter = require("../modules/caterer/caterer.router");
+const decoratorRouter = require("../modules/decorator/decorator.router");
 const makeupArtistRouter = require("../modules/makeupartist/makeupartist.router");
 const photographerRoutes = require("../modules/photographer/photographer.router");
 const userRouter = require("../modules/user/user.router");
@@ -8,4 +10,6 @@ router.use("/auth", authRoutes);
 router.use("/photographers", photographerRoutes);
 router.use("/users", userRouter);
 router.use("/makeup-artists", makeupArtistRouter);
+router.use('/caterers',catererRouter)
+router.use('/decorators',decoratorRouter)
 module.exports = router;
