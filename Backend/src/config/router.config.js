e3b64b011a06aaa8eb3167f7dcc1738137dbe3cc
Nischal Tandukar <nischaltan@gmail.com>
2025-06-20 12:53:48 +0545

    venue apis built and tested

--- Backend/src/config/router.config.js
+++ Backend/src/config/router.config.js
@@ -5,6 +5,7 @@ const decoratorRouter = require("../modules/decorator/decorator.router");
 const makeupArtistRouter = require("../modules/makeupartist/makeupartist.router");
 const photographerRoutes = require("../modules/photographer/photographer.router");
 const userRouter = require("../modules/user/user.router");
+const venueRouter = require("../modules/venue/venue.router");
 
 router.use("/auth", authRoutes);
 router.use("/photographers", photographerRoutes);
@@ -12,4 +13,5 @@ router.use("/users", userRouter);
 router.use("/makeup-artists", makeupArtistRouter);
 router.use('/caterers',catererRouter)
 router.use('/decorators',decoratorRouter)
+router.use('/venues', venueRouter);
 module.exports = router;
